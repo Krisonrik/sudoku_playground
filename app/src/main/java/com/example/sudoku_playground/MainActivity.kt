221e421cc5fb9e319f66d40e597bcfb7a91eeab6
Richard Yao <richard823@gmail.com>
2025-04-01 22:16:24 -0700

    hook up binding and assign random number to individual text and add a click listener to change value

--- app/src/main/java/com/example/sudoku_playground/MainActivity.kt
+++ app/src/main/java/com/example/sudoku_playground/MainActivity.kt
@@ -8,13 +8,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewbinding.ViewBinding
 import com.example.sudoku_playground.databinding.ActivityMainBinding
+import timber.log.Timber
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
         enableEdgeToEdge()
-        ActivityMainBinding.inflate(layoutInflater)?.let { binding ->
+        ActivityMainBinding.inflate(layoutInflater).let { binding ->
             setContentView(binding.root)
             fillAllValues(binding)
         }
@@ -29,10 +33,15 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             for (i in 0 until 9) {
                 for (j in 0 until 9) {
-//                    val resId = resources.getIdentifier("number_${i}_${j}", "id", packageName)
-//                    findViewById<TextView>(resId)?.apply {
-//                        text = Random.toString()
-//                    }
+                    val resId = resources.getIdentifier("number_${i+1}_${j+1}", "id", packageName)
+                    Timber.d("resId: $resId, i: $i, j: $j")
+                    findViewById<TextView>(resId)?.let { textView ->
+                        val randomValue = Random.nextInt(1, 10).toString()
+                        textView.text = randomValue
+                        textView.setOnClickListener {
+                            textView.text = Random.nextInt(1, 10).toString()
+                        }
+                    }
                 }
             }
 
