249586e5d303f5f9b12bd7ad15d74861a725440c
Richard Yao <richard823@gmail.com>
2025-04-04 22:48:02 -0700

    store values in blocks and grid

--- app/src/main/java/com/example/sudoku_playground/SudokuCreationViewModel.kt
+++ app/src/main/java/com/example/sudoku_playground/SudokuCreationViewModel.kt
@@ -15,6 +15,9 @@ private const val BLOCK_WIDTH = 3
 class SudokuCreationViewModel(private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO) :
     ViewModel() {
 
+    private val blocks: MutableMap<Pair<Int, Int>, Array<IntArray>> = mutableMapOf()
+    private val grid: Array<IntArray> = Array(9) { IntArray(9) }
+
     private val _digitUpdate: MutableSharedFlow<PositionalDigitStatus> = MutableSharedFlow()
     val digitUpdate: Flow<PositionalDigitStatus> = _digitUpdate.asSharedFlow()
 
@@ -24,6 +27,7 @@ class SudokuCreationViewModel(private val coroutineDispatcher: CoroutineDispatch
                 for (blockX in 0 until BLOCK_WIDTH) {
                     generateBlock().also {
                         Timber.d("block: ${it.joinToString { it.joinToString() }}")
+                        blocks[Pair(blockX, blockY)] = it
                         for (innerY in 0 until BLOCK_WIDTH) {
                             for (innerX in 0 until BLOCK_WIDTH) {
                                 val (mainBlockId, subIndex) = blockToId(
@@ -40,6 +44,21 @@ class SudokuCreationViewModel(private val coroutineDispatcher: CoroutineDispatch
                     }
                 }
             }
+            updateGrid()
+            printGrid()
+        }
+    }
+
+    private fun printGrid() {
+        Timber.d("Grid:")
+        grid.forEach {
+            Timber.d(it.joinToString(" ") { it.toString() })
+        }
+    }
+
+    private fun updateGrid() {
+        blocks.forEach {
+            fillGridWithBlock(it.value, it.key.first, it.key.second)
         }
     }
 
@@ -70,6 +89,26 @@ class SudokuCreationViewModel(private val coroutineDispatcher: CoroutineDispatch
         return Pair(mainBlockId, subIndex)
     }
 
+    private fun blockToGrid(
+        blockX: Int, blockY: Int, blockPositionX: Int, blockPositionY: Int
+    ): Pair<Int, Int> {
+        val gridX = blockX + BLOCK_WIDTH * blockPositionX
+        val gridY = blockY + BLOCK_WIDTH * blockPositionY
+        Timber.d("blockToGridIndex: blockX: $blockX, blockY: $blockY, blockPositionX: $blockPositionX, blockPositionY: $blockPositionY, gridX: $gridX, gridY: $gridY")
+        return Pair(gridX, gridY)
+    }
+
+    private fun fillGridWithBlock(
+        block: Array<IntArray>, blockPositionX: Int, blockPositionY: Int
+    ) {
+        for (blockY in 0 until BLOCK_WIDTH) {
+            for (blockX in 0 until BLOCK_WIDTH) {
+                val (gridX, gridY) = blockToGrid(blockX, blockY, blockPositionX, blockPositionY)
+                grid[gridY][gridX] = block[blockY][blockX]
+            }
+        }
+        Timber.d("fillGridWithBlock: blockPositionX: $blockPositionX, blockPositionY: $blockPositionY, block: ${block.joinToString { it.joinToString() }}")
+    }
 }
 
 class PositionalDigitStatus(
